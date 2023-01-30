2a1345f22ff8f9cc4fa52e0b57ba323657d6817d
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-01-31 00:59:41 +0600

    Filter Stock AMD Intel as Button in Products Page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Nav, Card, Col, Container, Row, ButtonGroup, Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import Product from '../Product/Product';
 
@@ -13,6 +13,15 @@ const Products = () => {
     return (
         <div>
             <Container>
+                <Row>
+                    <div className='d-flex justify-content-end mb-2'>
+                        <ButtonGroup size="sm">
+                            <Button active={false}>In Stock</Button>
+                            <Button active={false}>Intel</Button>
+                            <Button active={false}>AMD</Button>
+                        </ButtonGroup>
+                    </div>
+                </Row>
                 <Row>
                     {
                         products?.map(product => <Col key={product._id} xs={12} sm={6} md={6} lg={4} className='mb-4'>
